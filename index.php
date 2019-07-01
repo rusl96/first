490236ec5b7681451885c66bcf6919795d7746d4
rusl96 <izmaylov.rusl@yandex.ru>
2019-07-01 16:14:06 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h1><?php
+            include 'hello.php';
+            include 'result.php';
+            ?>
+        </h1>
+    </body>
+</html>
---
+++ result.php
@@ -0,0 +1,4 @@
+<?php
+    print_r ('Ваше имя' . $_POST['name']);
+    print_r ('Ваше имя' . $_POST['name']);
+?>
